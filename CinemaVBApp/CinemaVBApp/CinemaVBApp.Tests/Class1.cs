4874f62fa247d97a81713cc57414503d786c43d5
CallumMcFadgen <callum.mcfadgen@gmail.com>
2020-08-24 14:24:59 +1200

    inital commit

---
+++ CinemaVBApp/CinemaVBApp/CinemaVBApp.Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CinemaVBApp.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Adult_Before_5_Test()
+        {
+            var form1 = new Form();
+            decimal adult_before_5 = form1.Adult_Before_5(1, "person", "tuesday", 6);
+            Assert.AreEqual(14.5, adult_before_5);
+        }
+    }
+}
---
+++ CinemaVBApp/CinemaVBApp/CinemaVBApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CinemaVBApp
+{
+    public partial class Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        public decimal Adult_Before_5(int quantity, string person, string day, decimal time)
+        {
+            decimal total_price = 14.5M;
+            return total_price;
+        }
+
+        public decimal Adult_After_5(int quantity, string person, string day, int time)
+        {
+            var total_price = default(decimal);
+            return total_price;
+        }
+
+        private void Cinemas_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
